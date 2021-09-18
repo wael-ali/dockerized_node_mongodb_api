4cd928a332abbfed17cba458f68e81218fb0d4c9
Wael Ali <wael.ali@elbformat.de>
2021-09-18 23:16:49 +0200

    handling Not Found routes in Front end

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.svg">
+    <HelloWorld msg="404 Not Found"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'NotFound',
+  components: {
+    HelloWorld
+  }
+}
+</script>
