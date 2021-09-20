223ebfe5429821d53ebd7e40607d6f629c337837
Wael Ali <wael.ali@elbformat.de>
2021-09-21 00:43:13 +0200

    main grid css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,19 @@
       <router-link to="/about">About</router-link> |
       <router-link :to="{ path: 'todos' }">Todos</router-link>
     </div>
-    <router-view/>
+    <div class="flex">
+      <div class="bg-green-500 w-36">
+        side bar
+      </div>
+      <div class="text-center bg-red-300 min-h-screen w-full">
+        <router-view/>
+      </div>
+    </div>
+    <div class="  bg-blue-500">
+      <div> footer </div>
+      <div> footer </div>
+      <div> footer </div>
+    </div>
   </div>
 </template>
 
@@ -14,12 +26,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
+  @apply flex flex-col bg-red-500 bg-opacity-50;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  /* padding: 30px; */
+  @apply p-2 h-10 bg-yellow-300;
 }
 
 #nav a {
